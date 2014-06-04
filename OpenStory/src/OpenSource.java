5a2edf766ee0a44c6fee856fadea232dd57a97ac
hpepper <hpepper@spam.no>
2014-06-04 23:27:00 +0200

    Added Java project.

---
+++ OpenStory/src/OpenSource.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author hck
+ *
+ */
+public class OpenSource {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
