1a7596b6cbd52b636e14394c7575a94778fb8492
hpepper <hpepper@spam.no>
2014-06-06 21:13:35 +0200

    Add tabbed row to IdeaComponent.

--- OpenStoryCpp/Source/IdeaComponent.cpp
+++ OpenStoryCpp/Source/IdeaComponent.cpp
@@ -10,10 +10,16 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "IdeaComponent.h"
+#include "IdeaListComponent.h"
+#include "IdeaNewComponent.h"
 
 //==============================================================================
 IdeaComponent::IdeaComponent()
+: m_LocalTab(TabbedButtonBar::TabsAtTop)
 {
+    m_LocalTab.addTab("List", Colour(0,0,0), new IdeaListComponent(),  true);
+    m_LocalTab.addTab("New", Colour(0,0,0), new IdeaNewComponent(),  true);
+    addAndMakeVisible(m_LocalTab);
     // In your constructor, you should add any child components, and
     // initialise any special settings that your component needs.
 
@@ -28,5 +34,5 @@ void IdeaComponent::resized()
 {
     // This method is where you should set the bounds of any child
     // components that your component contains..
-
+   m_LocalTab.setBounds (getLocalBounds().reduced (4));
 }
